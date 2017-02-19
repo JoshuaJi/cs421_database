0227c9a7f0cc21e3bc172449f3cc4c19f34fbdb6
Cime Ajouz <cime.ajouz@mail.mcgill.ca>
2017-02-19 16:28:24 -0500

    Create question3.sql

---
+++ question3.sql
@@ -0,0 +1,5 @@
+INSERT INTO carmanufacturer  VALUES (111111, 'ralph', '900 Guy', 'Montreal', 'QC');
+INSERT INTO carmanufacturer  VALUES (222222, 'steven', '201 Lausanne', 'Montreal', 'QC');
+INSERT INTO carmanufacturer  VALUES (333333, 'joshua', '44 University', 'Toronto', 'ON');
+INSERT INTO carmanufacturer  VALUES (452637, 'cime', '876 Modugno', 'Montreal', 'QC');
+INSERT INTO carmanufacturer  VALUES (234567, 'sam', '543 Maisonneuve', 'Montreal', 'QC');
