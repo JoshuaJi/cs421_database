a3725fce26bf47973e13c480bfb58f5bfd5d270b
JohsuaJi <JiXu204@gmail.com>
2017-02-19 16:43:31 -0500

    add show tables and show table info sql

---
+++ show_tables.sql
@@ -0,0 +1,14 @@
+\d branchoffice;
+\d employee;
+\d mechanic;
+\d salesman;
+\d manager;
+\d customer;
+\d carmanufacturer;
+\d car;
+\d option;
+\d contract;
+\d transaction;
+\d maintenance;
+\d performs;
+\d features;
