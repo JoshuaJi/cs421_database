84d30cb4fff048dcd70a9457c9088afc0de4a9d9
Cime Ajouz <cime.ajouz@mail.mcgill.ca>
2017-02-19 16:39:12 -0500

    Update question3.sql

--- question3.sql
+++ question3.sql
@@ -1,5 +1,5 @@
-INSERT INTO carmanufacturer  VALUES (111111, 'ralph', '900 Guy', 'Montreal', 'QC');
-INSERT INTO carmanufacturer  VALUES (222222, 'steven', '201 Lausanne', 'Montreal', 'QC');
-INSERT INTO carmanufacturer  VALUES (333333, 'joshua', '44 University', 'Toronto', 'ON');
-INSERT INTO carmanufacturer  VALUES (452637, 'cime', '876 Modugno', 'Montreal', 'QC');
-INSERT INTO carmanufacturer  VALUES (234567, 'sam', '543 Maisonneuve', 'Montreal', 'QC');
+INSERT INTO carmanufacturer  VALUES (111111, 'BMW', '900 Guy', 'Montreal', 'QC');
+INSERT INTO carmanufacturer  VALUES (222222, 'Merceded', '201 Lausanne', 'Montreal', 'QC');
+INSERT INTO carmanufacturer  VALUES (333333, 'Toyota', '44 University', 'Toronto', 'ON');
+INSERT INTO carmanufacturer  VALUES (452637, 'Honda', '876 Modugno', 'Montreal', 'QC');
+INSERT INTO carmanufacturer  VALUES (234567, 'Nissan', '543 Maisonneuve', 'Montreal', 'QC');
