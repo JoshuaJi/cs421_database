9d030882f8dc562657b6a87051f8517c20e7934b
Cime Ajouz <cime.ajouz@mail.mcgill.ca>
2017-02-19 16:22:50 -0500

    Changed INTEGER to VARCHAR

--- table.sql
+++ table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE branchoffice (
-branchid 		INTEGER,
+branchid 		VARCHAR(3),
 streetaddress 		TEXT NOT NULL, 
 city 			TEXT NOT NULL, 
 province 		TEXT NOT NULL, 
@@ -8,10 +8,10 @@ PRIMARY KEY (branchid)
 
 
 CREATE TABLE employee (
-	employeeid 		INTEGER,
+	employeeid 		VARCHAR(6),
 	name  			TEXT NOT NULL,
 	salary 			INTEGER NOT NULL,
-	branchid 		INTEGER NOT NULL,
+	branchid 		VARCHAR(3) NOT NULL,
 	streetaddress 		TEXT NOT NULL,
 	city 			TEXT NOT NULL,
 	province 		TEXT NOT NULL,
@@ -21,20 +21,20 @@ CREATE TABLE employee (
 );
 
 CREATE TABLE mechanic ( 
-employeeid 			INTEGER,
+employeeid 			VARCHAR(6),
 PRIMARY KEY(employeeid),
 FOREIGN KEY(employeeid) REFERENCES employee(employeeid) 
 );
 
 CREATE TABLE salesman ( 
-employeeid	 		INTEGER,
+employeeid	 		VARCHAR(6),
 totalsales 			INTEGER NOT NULL CHECK (totalsales > -1),
 PRIMARY KEY(employeeid),
 FOREIGN KEY(employeeid) REFERENCES employee(employeeid) 
 );
 
 CREATE TABLE manager ( 
-employeeid 			INTEGER,
+employeeid 			VARCHAR(6),
 PRIMARY KEY(employeeid),
 FOREIGN KEY(employeeid) REFERENCES employee(employeeid) 
 );
@@ -42,17 +42,17 @@ FOREIGN KEY(employeeid) REFERENCES employee(employeeid)
 CREATE TABLE customer ( 
 driverslicense 			VARCHAR(13),
 name 				TEXT NOT NULL,
-phonenumber	 		INTEGER NOT NULL,
+phonenumber	 		VARCHAR(10) NOT NULL,
 email 				TEXT NOT NULL,
 streetaddress 			TEXT NOT NULL,
 city 				TEXT NOT NULL,
 province 			TEXT NOT NULL,
-governmentalid 		INTEGER NOT NULL,
+governmentalid 			VARCHAR(13) NOT NULL,
 PRIMARY KEY (driverslicense)
 );
 
 CREATE TABLE carmanufacturer (
-businessid 			INTEGER,
+businessid 			VARCHAR(6),
 name 				TEXT NOT NULL, 
 streetaddress 			TEXT NOT NULL, 
 city 				TEXT NOT NULL, 
@@ -74,8 +74,8 @@ mileage 			INTEGER NOT NULL,
 acceleration 			FLOAT NOT NULL,
 enginetype 			TEXT NOT NULL, 
 drivertype 			TEXT NOT NULL, 
-branchid 			INTEGER NOT NULL, 
-businessid 			INTEGER NOT NULL, 
+branchid 			VARCHAR(3) NOT NULL, 
+businessid 			VARCHAR(6) NOT NULL, 
 manufacturedsince 		DATE NOT NULL,
 PRIMARY KEY (vin),
 FOREIGN KEY(branchid) REFERENCES branchoffice(branchid) ,
@@ -84,19 +84,19 @@ FOREIGN KEY(businessid) REFERENCES carmanufacturer(businessid)
 
 
 CREATE TABLE option (
-	optionid 			INTEGER,
+	optionid 			VARCHAR(6),
 	optiontype 			TEXT NOT NULL,
 	price 				INTEGER NOT NULL CHECK (price > 0),
 	PRIMARY KEY (optionid)
 );
 
 CREATE TABLE contract (
-contractid 			INTEGER, 
+contractid 			VARCHAR(6), 
 expirydate 			DATE NOT NULL, 
 dateofinitiation 		DATE NOT NULL, 
 content 			TEXT NOT NULL, 
 price 				INTEGER NOT NULL CHECK (price > 0), 
-employeeid 			INTEGER NOT NULL, 
+employeeid 			VARCHAR(6) NOT NULL, 
 driverslicense 			TEXT NOT NULL, 
 vin 				TEXT NOT NULL,
 PRIMARY KEY (contractid),
@@ -107,30 +107,28 @@ FOREIGN KEY(vin) REFERENCES car(vin)
 
 
 CREATE TABLE transaction (
-	transactionid 				INTEGER,
+	transactionid 				VARCHAR(6),
 	paymenttype 				TEXT NOT NULL,
 	time 					TIMESTAMP NOT NULL,
 	transactiondate 			DATE NOT NULL,
-	accountid 				INTEGER NOT NULL,
-	amount 				INTEGER NOT NULL CHECK (amount > 0), 
-	contractid 				INTEGER NOT NULL,
+	accountid 				VARCHAR(6) NOT NULL,
+	amount 					INTEGER NOT NULL CHECK (amount > 0), 
+	contractid 				VARCHAR(6) NOT NULL,
 	PRIMARY KEY (transactionid),
 	FOREIGN KEY (contractid) REFERENCES contract(contractid)
 );
 
 CREATE TABLE maintenance(
-maintenanceid 			INTEGER, 
+maintenanceid 				VARCHAR(6), 
 maintenancetype 			TEXT NOT NULL, 
 maintenancedate 			DATE NOT NULL,
 PRIMARY KEY (maintenanceid)
 );
 
-
-
 CREATE TABLE performs(
 vin 					TEXT, 
-employeeid 				INTEGER, 
-maintenanceid 			INTEGER,
+employeeid 				VARCHAR(6), 
+maintenanceid 				VARCHAR(6),
 PRIMARY KEY (vin, employeeid, maintenanceid),
 FOREIGN KEY(vin) REFERENCES car(vin) ,
 FOREIGN KEY(employeeid) REFERENCES mechanic(employeeid),
@@ -139,7 +137,7 @@ FOREIGN KEY(maintenanceid) REFERENCES maintenance(maintenanceid)
 
 CREATE TABLE features(
 vin 					TEXT, 
-optionid 				INTEGER,
+optionid 				VARCHAR(6),
 PRIMARY KEY (vin, optionid),
 FOREIGN KEY(vin) REFERENCES car(vin) ,
 FOREIGN KEY(optionid) REFERENCES option(optionid)
