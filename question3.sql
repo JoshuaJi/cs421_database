2fdc6adc9448120cfc337b7109a1f39401a23d4d
steventir <steven.tir@mail.mcgill.ca>
2017-02-20 16:36:35 -0500

    added the remaining statements

--- question3.sql
+++ question3.sql
@@ -3,3 +3,5 @@ INSERT INTO carmanufacturer  VALUES ('222222', 'Merceded', '201 Lausanne', 'Mont
 INSERT INTO carmanufacturer  VALUES ('333333', 'Toyota', '44 University', 'Toronto', 'ON');
 INSERT INTO carmanufacturer  VALUES ('452637', 'Honda', '876 Modugno', 'Montreal', 'QC');
 INSERT INTO carmanufacturer  VALUES ('234567', 'Nissan', '543 Maisonneuve', 'Montreal', 'QC');
+
+SELECT * from carmanufacturer;
