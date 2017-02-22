0d10f6482ec3a1873857e0f709d31bd1a9517ef1
Cime Ajouz <cime.ajouz@mail.mcgill.ca>
2017-02-22 18:18:40 -0500

    Update question9.py

--- question9.py
+++ question9.py
@@ -1,1 +1,42 @@
+businessid = []
+name = []
+streetaddress = []
+city = ["Montreal", "Toronto", "Vancouver", "Ottawa", "Moncton", "Edmonton", "Victoria", "Orleans", "London", "Halifax", "St. John's", "Markham", "Richmond Hill"]
+province = ["QC", "ON", "BC", "ON", "NB", "AB", "NS", "NL"]
+def get_random_city(prov)
+  if prov == 'ON':
+    list = ['Toronto', 'Ottawa', 'Orleans', 'London', 'Richmond Hill']
+    return random.choise(list)
+    # generate random city from Toronto Ottawa Orleans London Richmond Hill
+  if prov == 'QC':
+    return 'Montreal'
+  if prov == 'BC':
+    return 'Vancouver'
+  if prov == 'NB':
+    return 'Moncton'
+  if prov == 'AB':
+    return 'Edmonton'
+  if prov == 'NS':
+    return 'Halifax'
+  if prov == 'NL':
+    return 'St. John\'s'
+if montreal
+province = qc
 
+
+vin = []
+description = []
+licenseplate = []
+price = []
+model = []
+color []
+year = []
+make = []
+fuel = []
+mileage = []
+acceleration = []
+enginetype = []
+drivertype = []
+branchid = ["001", "002", "003", "004", "005"]
+businessid = []
+manufacturedsince = []
