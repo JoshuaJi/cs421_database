18ab01c24dbf4883c8571d79f3eb1c5c79890b11
Cime Ajouz <cime.ajouz@mail.mcgill.ca>
2017-02-22 20:45:58 -0500

    Update question9.py

--- generate_random_data/question9.py
+++ generate_random_data/question9.py
@@ -22,46 +22,62 @@ provs = {
 	"Markham": "ON", 
 	"Richmond Hill": "ON"
 }
+
 def get_random_manufac_city():
 	city = random.choice(provs.keys())
 	return (city, provs[city])
+
 def get_random_manufac_businessid():
 	temp_businessid = randint(100000, 999999)
 	return temp_businessid
+
 def get_radom_manufac_streetaddress():
 	return gen_data.create_street()
-def get_random_carmanufac_name():
+
+def get_random_manufac_name():
 	return random.choice(name)
+
 def get_random_vin():
 	return vin.getRandomVin()
-vin = []
+
 description = ["New", "Used"]
 def get_random_car_description():
 	return random.choice(description)
 licenseplate = []
+
 def get_random_car_price():
 	return randint(10000, 120000)
+
 model = []
+
 color = ["Red", "Blue", "Green", "Yellow", "Gray", "White", "Black", "Pink", "Orange"]
 def get_random_car_color():
 	return random.choice(color)
+
 def get_random_car_year():
 	return randint(2005, 2017)
+
 make = []
+
 fuel = ["Supreme", "Regular", "Diesel"]
+
 def get_random_car_fuel():
 	return random.choice(fuel)
 
 def get_random_car_mileage():
 	return randint(0, 200000)
+
 def get_random_car_acceleration():
 	return randint(3, 15)
+
 enginetype = ["gas", "diesel"]
 def get_random_car_enginetype():
 	return random.choice(enginetype)
+
 drivertype = ["2-wheel", "4-wheel"]
 def get_random_car_drivertype():
 	return random.choice(drivertype)
+
 branchid = ["001", "002", "003", "004", "005"]
 def get_random_car_branchid():
 	return random.choice(branchid)
@@ -71,7 +87,6 @@ def generate_businessids():
 		for city in provs:
 			car_manufacturers[get_random_manufac_businessid()] = (car_m, get_radom_manufac_streetaddress(), city, provs[city])
 
-
 businessid = {
 	"BMW": 111111,
 	"Mercedes": 222222,
@@ -80,11 +95,9 @@ businessid = {
 	"Nissan": 234567
 	
 }
-manufacturedsince = []
 
 def generate_random_date():
 	temp_date = datetime.date(randint(2005,2017), randint(1,12),randint(1,28))
 	return temp_date.year+"-"+temp_date.month+"-"+temp_date.day
 
 generate_businessids()
-print car_manufacturers
